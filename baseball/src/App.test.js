8912629a1ea588bc26c1a4c29357252aec20e1dc
Josh Akeman <joshakeman@gmail.com>
2019-05-14 17:06:52 -0400

    first test for strikeout button works

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+
+  it('should display hello world', () => {
+    const { getByText } = render(<App />);
+
+    getByText(/dashboard/i);
+  });
+
+  it('should register strike count', () => {
+    const { getByText } = render(<App />);
+
+    const button = getByText(/X/i);
+    fireEvent.click(button);
+
+    const { queryAllByTestId } = render(<App />);
+    const strikes = queryAllByTestId('strikes')
+
+    expect(parseInt(strikes[0].textContent)).toEqual(1)
+  });
+  //
+  
 });
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Display(props) {
     return (
     <>
-        <h2>Strikes: {props.count.strikes}</h2>
+        <h2>Strikes: <div data-testid="strikes">{props.count.strikes}</div></h2>
         <h2>Balls: {props.count.balls}</h2>
     </>
     )
